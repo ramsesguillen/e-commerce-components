5e874beab76ec7792980217bc7fc8ff95d598354
RICHARD MICHEL ATECAS NOGALES <richardmichelatecasnogales@michelnogales-2.local>
2022-02-12 23:45:19 -0600

    add second way to build

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,2 @@
-export * from './components/index';
+export * as default from './components/index';
+
---
+++ types/stories/Button.stories.d.ts
@@ -0,0 +1,7 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+declare const _default: ComponentMeta<import("../components/helpers").BsPrefixRefForwardingComponent<"button", import("../components/Button").ButtonProps>>;
+export default _default;
+export declare const Primary: ComponentStory<import("../components/helpers").BsPrefixRefForwardingComponent<"button", import("../components/Button").ButtonProps>>;
+export declare const Secondary: ComponentStory<import("../components/helpers").BsPrefixRefForwardingComponent<"button", import("../components/Button").ButtonProps>>;
+export declare const Large: ComponentStory<import("../components/helpers").BsPrefixRefForwardingComponent<"button", import("../components/Button").ButtonProps>>;
+export declare const Small: ComponentStory<import("../components/helpers").BsPrefixRefForwardingComponent<"button", import("../components/Button").ButtonProps>>;
