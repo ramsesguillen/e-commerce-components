563848d41a57baa42d6e2288eba5ae6b1d2e42f5
RICHARD MICHEL ATECAS NOGALES <richardmichelatecasnogales@michelnogales-2.local>
2022-02-14 00:58:13 -0600

    fix: custom Button add feature FontAwesomeIcon Loading

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,6 +10,17 @@ import { useBootstrapPrefix } from './ThemeProvider';
 import { BsPrefixProps, BsPrefixRefForwardingComponent } from './helpers';
 import { ButtonVariant } from './types';
 
+const faAsterisk = {
+    prefix: 'fal',
+    iconName: 'asterisk',
+    icon: [
+        448,
+        512,
+        [10033, 61545],
+        '2a',
+        'M413.8 368.1C410.8 373.2 405.5 376 399.1 376c-2.766 0-5.562-.7187-8.125-2.219L240 284V464c0 8.844-7.156 16-15.1 16S208 472.8 208 464V284l-151.9 89.73C53.58 375.3 50.79 376 48.02 376c-5.469 0-10.8-2.812-13.8-7.859c-4.484-7.609-1.969-17.42 5.641-21.92L192.5 256L39.86 165.8c-7.609-4.5-10.12-14.31-5.641-21.92c4.531-7.578 14.31-10.06 21.92-5.641L208 227.1V48C208 39.16 215.2 32 224 32S240 39.16 240 48v179.1l151.9-89.73c7.641-4.422 17.42-1.938 21.92 5.641c4.484 7.609 1.969 17.42-5.641 21.92L255.5 256l152.7 90.22C415.7 350.7 418.3 360.5 413.8 368.1z',
+    ],
+};
 export interface ButtonProps
     extends BaseButtonProps,
         Omit<BsPrefixProps, 'as'> {
@@ -57,10 +68,7 @@ const propTypes = {
      * Specifies class icon.
      *
      */
-    icon: PropTypes.oneOfType([
-        PropTypes.string,
-        PropTypes.object
-      ]),
+    icon: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
 
     /** Manually set the visual state of the button to `:active` */
     active: PropTypes.bool,
@@ -90,7 +98,7 @@ const defaultProps = {
     variant: 'primary',
     active: false,
     disabled: false,
-    loading: false
+    loading: false,
 };
 
 export const Button: BsPrefixRefForwardingComponent<'button', ButtonProps> =
@@ -118,18 +126,26 @@ export const Button: BsPrefixRefForwardingComponent<'button', ButtonProps> =
             });
 
             const Component = tagName as React.ElementType;
-            
-            if(loading){
+
+            if (loading) {
                 props.disabled = true;
-                icon = 'fal fa-asterisk fa-spin';
+                icon = faAsterisk; /* 'fal fa-asterisk fa-spin'; traditional */
             }
-            
+
             const getIcon = (valueIcon: any) => {
-                if( typeof valueIcon === 'string'){
-                    return (<i className={valueIcon}></i>);
+                if (typeof valueIcon === 'string') {
+                    return <i className={valueIcon}></i>;
                 }
-                return (<FontAwesomeIcon icon={valueIcon} />);
-            }
+
+                const classNameSpin = classNames(loading && 'fa-spin');
+
+                return (
+                    <FontAwesomeIcon
+                        icon={valueIcon}
+                        className={classNameSpin}
+                    />
+                );
+            };
 
             return (
                 <Component
@@ -146,7 +162,7 @@ export const Button: BsPrefixRefForwardingComponent<'button', ButtonProps> =
                         loading && 'disabled'
                     )}
                 >
-                    {icon && getIcon(icon) } { label ? label : children }
+                    {icon && getIcon(icon)} {label ? label : children}
                 </Component>
             );
         }
